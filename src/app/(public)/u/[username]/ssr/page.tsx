7ae6d4e0b827ab6ff9322727983eaac5b7435640
HolohotsIhor <gologocigor@gmail.com>
2026-04-03 00:00:07 +0300

    feat: add title

--- src/app/(public)/u/[username]/ssr/page.tsx
+++ src/app/(public)/u/[username]/ssr/page.tsx
@@ -1,10 +1,16 @@
+import type { Metadata } from 'next';
+
 type Params = {username?: string};
 
-export default async function PageUsername({
-    params
-}: {
-    params: Promise<Params>
-}) {
+export async function generateMetadata({ params }: { params: Promise<{ username: string }> }): Promise<Metadata> {
+  const { username } = await params;
+
+  return {
+    title: '@' + (await params).username,
+  };
+}
+
+export default async function PageUsername({ params }: { params: Promise<Params> }) {
   const { username } = await params
   
   return <h1 className='text-3xl mb-3'>Username: {username ? username : 'need to add username'}</h1>
